c5c5df33c246ac48596c86dff7aa8d081bb4262c
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2020-11-21 10:54:53 +0900

    React Week 2 Propsの内容を更新

---
+++ src/books.js
@@ -0,0 +1,88 @@
+export const books = [
+  {
+    id: 1,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/71FXfPs-iyL._AC_UL200_SR200,200_.jpg',
+    title: 'Where the Crawdads Sing',
+    author: 'Delia Owen',
+  },
+  {
+    id: 2,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/81yBRyp9dIL._AC_UL200_SR200,200_.jpg',
+    title: 'Code Name Bananas: The hilarious and epic new…',
+    author: 'David Walliams',
+  },
+  {
+    id: 3,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/81pUabSLq4L._AC_UL200_SR200,200_.jpg',
+    title: 'The Thursday Murder Club: The Record-Breaking…',
+    author: 'Richard Osman',
+  },
+  {
+    id: 4,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/613rMCldkvL._AC_UL200_SR200,200_.jpg',
+    title: 'The Ickabog',
+    author: 'J.K. Rowling',
+  },
+  {
+    id: 5,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/81fKY3dmCCL._AC_UL200_SR200,200_.jpg',
+    title: 'The Boy, The Mole, The Fox and The Horse',
+    author: 'Charlie Mackesy',
+  },
+  {
+    id: 6,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/71BGN0W4COL._AC_UL200_SR200,200_.jpg',
+    title: 'Cook, Eat, Repeat: Ingredients, recipes and stories.',
+    author: 'Nigella Lawson',
+  },
+  {
+    id: 7,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/81B0tZK01FL._AC_UL200_SR200,200_.jpg',
+    title: 'Diary of a Wimpy Kid: The Deep End (Book 15)',
+    author: 'Jeff Kinney',
+  },
+  {
+    id: 8,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/81hWbMi%2BU2L._AC_UL200_SR200,200_.jpg',
+    title: 'Ottolenghi FLAVOUR',
+    author: 'Yotam Ottolenghi ',
+  },
+  {
+    id: 9,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/81J6APjwxlL._AC_UL200_SR200,200_.jpg',
+    title: 'The Midnight Library',
+    author: 'Matt Haig',
+  },
+  {
+    id: 10,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/91ldba14t6L._AC_UL200_SR200,200_.jpg',
+    title: "Troy: Our Greatest Story Retold (Stephen Fry's Greek Myths)",
+    author: 'Stephen Fry',
+  },
+  {
+    id: 11,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/91%2BNBrXG-PL._AC_UL200_SR200,200_.jpg',
+    title: 'A Promised Land',
+    author: 'Barack Obama',
+  },
+  {
+    id: 12,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/91z1Su7ZEtL._AC_UL200_SR200,200_.jpg',
+    title: 'The Danger Gang',
+    author: 'Tom Fletcher',
+  },
+]
+
+// export default books;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,43 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
+import { books } from './books'
 
 function BookList() {
   return (
     <div>
-<h1 style={{textAlign:'center', paddingTop:30}}>Over 40 Web Club Booklist</h1>
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
+      <h1 style={{ textAlign: 'center', paddingTop: 30 }}>
+        Over 40 Web Club Booklist
+      </h1>
+      <section className='booklist'>
+        {books.map((book) => (
+          <Book key={book.id} {...book} />
+        ))}
+      </section>
     </div>
   )
 }
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
+  const clickHandler = () => {
+    alert('Viva, Over 40 Web Club!!')
+  }
+  const complexClickHandler = (author) => {
+    console.log(author)
+  }
   return (
     <section className='book'>
-      <img src="https://images-eu.ssl-images-amazon.com/images/I/71FXfPs-iyL._AC_UL200_SR200,200_.jpg" alt="" />
-      <h2>Where the Crawdads Sing</h2>
-      <h4>Delia Owen</h4>
+      <img src={img} alt='' />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        clickHandler
+      </button>
+      <button type='button' onClick={() => complexClickHandler(author)}>
+        complexClickHandler
+      </button>
     </section>
-
-
-)
-
+  )
 }
 
 ReactDom.render(<BookList />, document.getElementById('root'))
